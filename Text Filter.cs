cdcd48d10d2f2e0cebbc9ee8318456d1062a78db
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-09 00:33:48 +0200

    Add files via upload

---
+++ Text Filter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _4._Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            string replacedText = string.Empty;
+            for (int i = 0; i < banWords.Length; i++)
+            {
+                string asterix = string.Empty;
+                for (int j = 0; j < banWords[i].Length; j++)
+                {
+                    asterix += '*';
+                }
+                replacedText = text.Replace(banWords[i], asterix);
+                text = replacedText;
+            }
+            Console.WriteLine(replacedText);
+        }
+    }
+}
