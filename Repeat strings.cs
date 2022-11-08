c8bcf67110c1f81886b61f09f9d5af68b0aa9bb5
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-11-09 00:32:24 +0200

    Add files via upload

---
+++ Repeat strings.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace ConsoleApp1
+{
+    using System;
+    using System.Text;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = string.Empty;
+            string[] textArray = Console.ReadLine().Split();
+            foreach (string word in textArray)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    result += word;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
